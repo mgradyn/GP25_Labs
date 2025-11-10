51e0190ab3761373116d46f32ac1b91899e170e9
Alireza <alireza.lm69@gmail.com>
2025-11-10 15:46:14 +0200

    lab 2 added

---
+++ Lab_2/kernel.cu
@@ -0,0 +1,10 @@
+__global__ void vecAddKernel(float *A, float *B, float *C, int n) {
+
+  // Calculate global thread index based on the block and thread indices ----
+
+  // INSERT KERNEL CODE HERE
+
+  // Use global index to determine which elements to read, add, and write ---
+
+  // INSERT KERNEL CODE HERE
+}
