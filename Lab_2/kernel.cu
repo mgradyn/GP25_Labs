cc64564fb998998d6c8c47986d2c124dd9cd279c
Martinus Grady <48128978+mgradyn@users.noreply.github.com>
2025-11-15 01:15:42 +0200

    Add vector addition logic to vecAddKernel
    Implemented vector addition in the CUDA kernel.

--- Lab_2/kernel.cu
+++ Lab_2/kernel.cu
@@ -2,9 +2,11 @@ __global__ void vecAddKernel(float *A, float *B, float *C, int n) {
 
   // Calculate global thread index based on the block and thread indices ----
 
-  // INSERT KERNEL CODE HERE
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
 
   // Use global index to determine which elements to read, add, and write ---
 
-  // INSERT KERNEL CODE HERE
+  if (i < n) {
+    C[i] = A[i] + B[i];
+  }
 }
